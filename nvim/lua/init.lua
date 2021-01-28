705d8251d2b4ce6f9493a3945cb0f648102b5c84
Mauricio Trajano <mauriciot1993@gmail.com>
2021-01-27 21:45:08 -0500

    fixed treesitter indent breaking in php

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,11 +1,8 @@
 require'nvim-treesitter.configs'.setup {
   ensure_installed = {"php", "python", "javascript", "tsx", "json", "lua"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = {},  -- list of language that will be disabled
-  },
-  indent = {
     enable = true,
-    disable = {},  -- list of language that will be disabled
-  }
+    disable = {"php"}, -- breaks autoindent in php
+  },
+  indent = { enable = true },
 }
