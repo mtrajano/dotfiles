66401ebaa7c5cb4009af2d3a8cd0d3fa05bfb86a
Mauricio Trajano <mauriciot1993@gmail.com>
2021-04-17 20:32:18 -0400

    projections for be.net and pro2-view

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,7 +1,17 @@
 # alacritty
-ln -is $PWD/alacritty $XDG_CONFIG_HOME/
+ln -is alacritty $XDG_CONFIG_HOME/
 
-ln -is $PWD/kitty $XDG_CONFIG_HOME/
+ln -is kitty $XDG_CONFIG_HOME/
 
 # nvim
-ln -is $PWD/nvim/{ftplugin,snips,after,lua} $XDG_CONFIG_HOME/nvim
+ln -is nvim/{ftplugin,snips,after,lua} $XDG_CONFIG_HOME/nvim
+
+# create projections
+for dir in nvim/projections/*
+do
+  file="$dir/projections.json"
+  repo=${dir##*/}
+
+  ln -i $file "$HOME/dev/behance/$repo/.projections.json"
+done
+
