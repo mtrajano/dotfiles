ef61d792d9c04df6dd3a28eeeb01f050b38642a0
Mauricio Trajano <mauriciot1993@gmail.com>
2022-09-03 21:51:19 -0400

    converted fugitive to lua

---
+++ .config/nvim/ftplugin/fugitive.lua
@@ -0,0 +1,17 @@
+local function map(mode, lhs, rhs, options)
+  options = options or {}
+  options.buffer = true
+  vim.keymap.set(mode, lhs, rhs, options)
+end
+
+map({ 'n', 'v' }, '<tab>', '=', { remap = true })
+
+-- p for pull
+map('n', 'p', function()
+  vim.cmd.G('pu')
+end)
+
+-- P for push
+map('n', 'P', function()
+  vim.cmd.G('up')
+end)
--- .config/nvim/ftplugin/fugitive.vim
+++
@@ -1,5 +0,0 @@
-" toggle diffs with tab
-nmap <buffer> <tab> =
-vmap <buffer> <tab> =
-" easy push
-nnoremap <buffer> P :G up<cr>
