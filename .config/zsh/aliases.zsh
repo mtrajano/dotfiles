a7745010b4b0459035614e0b47e17d93656e00e0
Mauricio Trajano <mauriciot1993@gmail.com>
2024-08-10 12:01:20 -0400

    yazi cd and google cloud cli

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -143,6 +143,18 @@ ta() {
   fi
 }
 
+######
+# YAZI
+######
+function yy() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
+
 ##################
 # COMPOSER ALIASES
 ##################
--- .config/zsh/zshrc.zsh
+++ .config/zsh/zshrc.zsh
@@ -32,5 +32,11 @@ source "$XDG_CONFIG_HOME/zsh/completion.zsh"
 source "$XDG_CONFIG_HOME/zsh/keybindings.zsh"
 source "$XDG_CONFIG_HOME/zsh/aliases.zsh"
 
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/mtrajano/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/mtrajano/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/mtrajano/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/mtrajano/google-cloud-sdk/completion.zsh.inc'; fi
+
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
